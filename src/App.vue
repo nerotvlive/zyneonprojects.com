f3c39a016b969bf882ff3accfcaf78ce0121f926
nerotvlive <daniel.niesmann@outlook.com>
2026-05-08 19:29:50 +0200

    Remove HelloWorld component and its assets; update App.vue with placeholder content

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <div class="container mx-auto">
+    WORK IN PROGRESS
+  </div>
 </template>
