bd270a4a64e232a98b960ebb1d07f7ae217e7d4b
nerotvlive <daniel.niesmann@outlook.com>
2026-05-14 17:15:43 +0200

    Remove placeholder content and update dark mode background color

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,11 +3,6 @@
 
 <template>
   <div class="container min-h-screen max-h-screen mx-auto p-4 border-r border-l border-gray-800 bg-gray-950">
-    Zyneon Projects
-    <br>
-    <br>
-    <hr class="border-gray-700">
-    <br>
-    UNDER CONSTRUCTION...
+
   </div>
 </template>
